bbabd8859cb3b604d0277e3dd770e1907eaf0512
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-31 12:42:31 -0500

    Agregrando Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,18 @@
 import React from "react"
 import Header from "../components/header"
+import Helmet from "react-helmet"
 
 const Layout = props => {
   return (
     <>
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header />
       {props.children}
     </>
