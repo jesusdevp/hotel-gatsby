56fd82656e8525c46f0b4c3a26f61b660186b495
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-31 15:49:27 -0500

    CSS ala imagen principal y al hero

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -1,5 +1,41 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import styled from "@emotion/styled"
+
+const ImageBackground = styled(BackgroundImage)`
+  height: 850px;
+`
+
+const TextoImagen = styled.div`
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.4),
+    rgba(0, 0, 0, 0.4)
+  );
+  color: #fff;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-size: 4rem;
+    margin: 0;
+
+    @media (min-width: 992px) {
+      font-size: 5.8rem;
+    }
+  }
+  p {
+    font-size: 2rem;
+    @media (min-width: 992px) {
+      font-size: 2.6rem;
+    }
+  }
+`
 
 const ImagenHotel = () => {
   const { image } = useStaticQuery(graphql`
@@ -7,14 +43,20 @@ const ImagenHotel = () => {
       image: file(relativePath: { eq: "1.jpg" }) {
         sharp: childImageSharp {
           fluid {
-            srcSetWebp
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
     }
   `)
-  console.log(image)
-  return <h1>Imagen aqui</h1>
+  // console.log(image.sharp.fluid)
+  return (
+    <ImageBackground tag="section" fluid={image.sharp.fluid} fadeIn="soft">
+      <TextoImagen>
+        <h1>Bienvenido a Hotel Gatsby</h1>
+        <p>El mejor lugar para ti y tu familia</p>
+      </TextoImagen>
+    </ImageBackground>
+  )
 }
-
 export default ImagenHotel
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,9 +12,11 @@ const Layout = props => {
             font-size: 62.5%;
           }
           body {
-            font-size: 16px;
-            font-size: 1.6rem;
+            font-size: 18px;
+            font-size: 1.8rem;
             line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
+            overflow-y: hidden;
           }
           h1,
           h2,
