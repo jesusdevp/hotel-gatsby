9cdc272295ccbf64f7d217ddaaf2e1a89e2def59
Jesus Ramirez <jesusdevelp@gmail.com>
2020-08-03 23:39:21 -0500

    Agregando DatoCMS a Gatsby

---
+++ src/components/contenidoInicio.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const ContenidoInicio = () => {
+  const informacion = useStaticQuery(graphql`
+    query {
+      allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
+        nodes {
+          titulo
+          contenido
+          imagen {
+            fluid {
+              ...GatsbyDatoCmsFluid
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(informacion)
+  return (
+    <>
+      <h2>Titulo pagina</h2>
+    </>
+  )
+}
+
+export default ContenidoInicio
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
+import ContenidoInicio from "../components/contenidoInicio"
 
 const IndexPage = () => (
   <Layout>
     <ImagenHotel />
+    <ContenidoInicio />
   </Layout>
 )
 
