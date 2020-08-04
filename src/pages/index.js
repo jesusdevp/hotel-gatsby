065f05d3d153e05c636929d91e84d5b85be03d71
Jesus Ramirez <jesusdevelp@gmail.com>
2020-08-04 00:50:05 -0500

    Iterando en Habitaciones y Mostrando la informacion del componente

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
 import React from "react"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
 import ContenidoInicio from "../components/contenidoInicio"
-import useHabitciones from "../hooks/useHabitaciones"
+import HabitacionPreview from "../components/habitacionPreview"
+import useHabitaciones from "../hooks/useHabitaciones"
 
 const IndexPage = () => {
-  useHabitciones()
+  const habitaciones = useHabitaciones()
+
+  console.log(habitaciones)
   return (
     <Layout>
       <ImagenHotel />
       <ContenidoInicio />
+
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >
+        Nuestras Habitaciones
+      </h2>
+      <ul>
+        {habitaciones.map(habitacion => (
+          <HabitacionPreview key={habitacion.id} habitacion={habitacion} />
+        ))}
+      </ul>
     </Layout>
   )
 }
